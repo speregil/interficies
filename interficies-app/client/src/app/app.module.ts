10f46f525f1f2932d546a9c8b9a1211fbb0af216
bastianhogan <s.gilparga@gmail.com>
2019-05-09 15:26:18 -0500

    Pagina de registro agregada

--- interficies-app/client/src/app/app.module.ts
+++ interficies-app/client/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { RegistroComponent } from './principal/registro/registro.component';
@@ -31,7 +32,8 @@ import { AppRoutingModule } from './/app-routing.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
