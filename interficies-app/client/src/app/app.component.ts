08e19988858ccf394ebb9c1f30cbf349ec15cce6
bastianhogan <s.gilparga@gmail.com>
2019-07-03 14:50:46 -0500

    Agregada navegación hacia el home, dependiendo del progreso del usuario

--- interficies-app/client/src/app/app.component.ts
+++ interficies-app/client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from "@angular/router";
 import { RegistroService } from './principal/registro/registro.service';
 import { UserService } from './models/user.service';
 import { User } from './models/user.model';
@@ -10,22 +11,35 @@ import { LoginObserver } from './models/loginObserver.interface';
   styleUrls: ['./app.component.css']
 })
 
+/**
+ * Componente que controla la barra principal de la página
+ */
 export class AppComponent {
   
-  loginObservers: LoginObserver[];
+  loginObservers: LoginObserver[];   // Lista de componentes que necesitan ser notificados cuando se realiza un login
 
-  username = "";
-  password = "";
-  loggedUser = null;
+  username = "";      // Nombre del usuario que desea ingresar
+  password = "";      // Password del usuario que desea ingresar
+  loggedUser = null;  // Objeto que representa la infromación de un usuario loggeado
 
+//-------------------------------------------------------------------------------------------------------------------------------
+// Constructor
+//-------------------------------------------------------------------------------------------------------------------------------
 
-  constructor(private registro: RegistroService, private userService: UserService){
+  constructor(private registro: RegistroService, private userService: UserService, private router: Router){
     this.loginObservers = new Array();
     if(this.isLogged()) {
       this.updateLogin();
     }
   }
 
+//-------------------------------------------------------------------------------------------------------------------------------
+// Funciones
+//-------------------------------------------------------------------------------------------------------------------------------
+
+  /**
+   * Maneja el proceso de ingreso seguro al portal y actualiza la información en la barra
+   */
   login(){
     this.registro.login(this.username, this.password).subscribe(data => {
       if(data['status'] > 0 )
@@ -41,18 +55,35 @@ export class AppComponent {
           this.username = "";
           this.password = "";
 
-          this.notifyLogin(true);
+          this.notifyLogin(true);  //Notifica que hubo un login a todos los observadores
         });
       }     
     });
   }
 
+  /**
+   * Captura la información necesaria para ejecutar un login que otro componente ha solicitado
+   * @param user Nombre de usuario
+   * @param pass Password
+   */
   externalLogin(user, pass) {
     this.username = user;
     this.password = pass;
     this.login();
   }
+  /**
+   * Actualiza el usuario en sesión si se ha hecho un login externo
+   */
+  updateLogin() {
+    this.loggedUser = this.userService.getUserLoggedIn();
 
+    this.username = "";
+    this.password = "";
+  }
+
+  /**
+   * Termina la sesión de un usuario y notifica el proceso a todos los observadores registrados
+   */
   logout(){
     this.userService.setUserLoggedOut();
     this.loggedUser = null;
@@ -60,9 +91,30 @@ export class AppComponent {
     this.notifyLogin(false);
   }
 
+  /**
+   * Comunica si hay un usuario loggeado en el sistema actualmente
+   */
   isLogged(){
     return this.userService.isUserLogged();
   }
+
+  goHome(){
+    if(this.loggedUser) {
+      if(this.loggedUser.currentRol != 'Ninguno'){
+        this.router.navigate(["roles"]);
+      }
+      else{
+        this.router.navigate(["portada"]);
+      }
+    }
+    else{
+      this.router.navigate(["portada"]);
+    }
+  }
+
+  /**
+   * Funciones para obtener la información del usuario y de su progreso del objeto conservado en memoria
+   */
 
   getShownName(){
     if(this.loggedUser) {
@@ -96,13 +148,10 @@ export class AppComponent {
       return "";
   }
 
-  updateLogin() {
-    this.loggedUser = this.userService.getUserLoggedIn();
-
-    this.username = "";
-    this.password = "";
-  }
-
+  /**
+   * Actualiza el rol del usuario en sesión si otro componente ha solicitado el cambio
+   * @param role Nuevo rol del usuario
+   */
   updateRole( role ) {
     var currentUser = this.userService.getUserLoggedIn();
     this.userService.updateRole(currentUser.username, role).subscribe(response => {
@@ -115,10 +164,18 @@ export class AppComponent {
     });
   }
 
+  /**
+   * Agrega un nuevo observador a la lista de componenetes que deben ser notificados de un login
+   * @param newObserver Componente que implementa la interfaz de observador
+   */
   addLoginObserver (newObserver: LoginObserver) {
     this.loginObservers.push(newObserver);
   }
 
+  /**
+   * Notifica a todos los observadores de un login
+   * @param logged Determina si la notificación es de login o logout
+   */
   private notifyLogin(logged: boolean) {
     for (var observer of this.loginObservers) {
         observer.notifyLogin(logged);
