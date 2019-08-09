5febc1cec967757fe5e2287058b0ec051bcab6c8
sgilp <s.gilparga@gmail.com>
2019-08-08 22:21:11 -0500

    Vuelo de mosca completo

--- interficies-app/client/src/app/juglar/juglar.component.ts
+++ interficies-app/client/src/app/juglar/juglar.component.ts
@@ -13,19 +13,70 @@ export class JuglarComponent {
   
   masterChallenges = [];
 
-  retoActual = "RETO";
-  msnAceptar = "Aceptado";
+  retoActual = "";
+  msnAceptar = "";
   basico = true;
   basicAble = true;
-  masterAble = true;
-  acceptAble = true;
+  masterAble = false;
+  acceptAble = false;
 
   poolBasico = "selected";
   poolMaster = "juglar-button";
 
 
 
-  constructor(private userService: UserService, private router: Router, private http: HttpClient, private challenges: ChallengesService) {}
+  constructor(private userService: UserService, private router: Router, private http: HttpClient, private challenges: ChallengesService) {
+    challenges.getMasterChallenges('juglar').subscribe(response => {
+      if(response['mensaje'] == null && response['list'].length > 0){
+        this.masterChallenges = response['list'];
+        this.masterAble = true;
+      }
+    });
+  }
+
+  onFolderClick( folder ) {
+    if(this.userService.isUserLogged()) {
+      this.retoActual = "Procesando...";
+      this.acceptAble = false;
+      if(this.basico)
+        this.getBasicChallenge( folder );
+      else
+        this.getMasterChallenge();
+    }
+  }
+
+  getBasicChallenge( folder ){
+    this.http.get('assets/static/juglar/config.json', {responseType: 'json'}).subscribe(data => {
+      var len = parseInt(data["num"]);
+      var i = Math.floor(Math.random() * len) + 1;
+      this.http.get('assets/static/juglar/' + folder + '/' + folder + i + '.txt', {responseType: 'text'}).subscribe(txt => {
+        this.retoActual = txt;
+        this.acceptAble = true;
+      });
+    });
+  }
+
+  getMasterChallenge(){
+    var i = Math.floor(Math.random() * this.masterChallenges.length);
+    var challenge = this.masterChallenges[i];
+    this.retoActual = challenge.text;
+    this.acceptAble = true;
+  }
+
+  onAccept() {
+    if(this.basicAble) {
+      var user = this.userService.getUserLoggedIn();
+      this.userService.addChallenge(user.username, 'juglar', this.retoActual).subscribe(response => {
+        if(response['mensaje'])
+          this.msnAceptar = response['mensaje'];
+        else {
+          this.msnAceptar = 'Reto Aceptado';
+          this.basicAble = false;
+          this.masterAble = false;
+        }
+      });
+    }
+  }
 
   onContinue() {
     if(this.userService.isUserLogged()) {
