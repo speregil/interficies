9967c6e65e999dbc69fadde118e9f4efdf9a2e9c
bastianhogan <s.gilparga@gmail.com>
2019-09-06 16:30:01 -0500

    arreglos esteticos al master

--- interficies-app/master/src/app/registro/registro.component.ts
+++ interficies-app/master/src/app/registro/registro.component.ts
@@ -13,26 +13,32 @@ export class RegistroComponent {
 
   username = "";
   password = "";
+  confirm = "";
   shownName = "";
   msn = "";
 
   constructor(private service: UserService, private router: Router){}
 
   register(){
-    this.service.register(this.username, this.password, this.shownName).subscribe(data => {
-      if(data['status'] > 0 )
-        this.msn = data['mensaje'];
-      else{
-        this.service.login(this.username, this.password).subscribe(d => {
-          var userModel = d["data"];
-          var user : User = new User(this.username, userModel["shownName"]);        
-          this.service.setUserLoggedIn(user);
-          this.username = "";
-          this.password = "";
-          this.shownName = "";
-          this.router.navigate(['principal']);
-        });
-      }
-    });
+    this.msn = "";
+    if(this.password == this.confirm) {
+      this.service.register(this.username, this.password, this.shownName).subscribe(data => {
+        if(data['status'] > 0 )
+          this.msn = data['mensaje'];
+        else{
+          this.service.login(this.username, this.password).subscribe(d => {
+            var userModel = d["data"];
+            var user : User = new User(this.username, userModel["shownName"]);        
+            this.service.setUserLoggedIn(user);
+            this.username = "";
+            this.password = "";
+            this.shownName = "";
+            this.router.navigate(['principal']);
+          });
+        }
+      });
+    }
+    else
+      this.msn = "Las claves no coinciden";;
   }
  }
