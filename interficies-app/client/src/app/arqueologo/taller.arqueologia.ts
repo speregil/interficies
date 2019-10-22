6fa18864c8543cdba4a5142fd892ecd344067bdc
bastianhogan <s.gilparga@gmail.com>
2019-10-22 15:27:54 -0500

    Sesion de ajustes # 3

--- interficies-app/client/src/app/arqueologo/taller.arqueologia.ts
+++ interficies-app/client/src/app/arqueologo/taller.arqueologia.ts
@@ -2,20 +2,35 @@ import { Component } from '@angular/core';
 import { Router } from "@angular/router";
 import { UserService } from '../models/user.service';
 import { DownloadService } from '../models/downloads.service';
+import { MusicService } from 'src/app/models/music.service';
+import { AppComponent } from 'src/app/app.component';
 
 @Component({
   selector: 'arqueologia-taller',
   templateUrl: './taller.arqueologia.html',
   styleUrls: ['./arqueologo.component.css']
 })
+/**
+ * Componente que controla la asignación del taller del arqueología al usuario
+ */
 export class TallerComponent {
  
-  basicAble = true;
-  cargando = true;
-  msn = "Para comenzar la activdad, presiona ACEPTAR";
+  //------------------------------------------------------------------------------------------
+  // Campos y Atributos
+  //------------------------------------------------------------------------------------------
 
-  constructor(private userService: UserService, private router: Router, private download: DownloadService) {
+  basicAble = true;                                       // Determina si es posible aceptar el taller o no
+  cargando = true;                                        // Determina si la pagina está cargando contenido o no
+  msn = "Para comenzar la activdad, presiona ACEPTAR";    // Campo que guarda el estado del procedimiento actual
+
+  //------------------------------------------------------------------------------------------
+  // Constructor
+  //------------------------------------------------------------------------------------------
+
+  constructor(private userService: UserService, private router: Router, private download: DownloadService, music: MusicService, private principal: AppComponent) {
     this.msn = "Cargando";
+    music.setBg("");
+    principal.notifyBgChange(); 
     var user = this.userService.getUserLoggedIn();
     this.userService.getProgressState(user.username, 'tallerAsig').subscribe(response => {
         if(response['flag']){
@@ -26,12 +41,20 @@ export class TallerComponent {
     });
   }
 
+  //------------------------------------------------------------------------------------------
+  // Funciones
+  //------------------------------------------------------------------------------------------
+
+  /**
+   * Procedimiento para descargar el taller de exploración y crear el reto en la lista del usuario en sesion
+   * Reporta el estado del procedimiento en el campo msn
+   */
   onAccept(){
     if(this.basicAble && !this.cargando) {
       var user = this.userService.getUserLoggedIn();
       this.msn = "Espera un momento por favor...";
       this.cargando = true;
-      this.userService.addChallenge(user.username, 'taller', "Taller de la obra JAR").subscribe(response => {
+      this.userService.addChallenge(user.username, 'taller', "Debes explorar los contenidos digitales rescatados de unos antiguos servidores que se creian perdidos").subscribe(response => {
         if(response['mensaje'])
           this.msn = response['mensaje'];
         else {
@@ -47,6 +70,10 @@ export class TallerComponent {
     }
   }
 
+  /**
+   * Procedimiento para descargar el documento con las instrucciones del taller
+   * Reporta el estado del procedimiento en el campo msn
+   */
   onDownload(){
     var url = './assets/static/laboratorio/taller.docx';
     this.msn = "Reto Aceptado"
@@ -55,5 +82,12 @@ export class TallerComponent {
     this.download.downloadFile(url).subscribe(response => {
       window.location.href = response.url;
 		}), error => this.msn = error
+  }
+
+  /**
+   * Navega al menu principal
+   */
+  onContinue(){
+    this.router.navigate(['roles']);
   }
 }
--- interficies-app/client/src/app/principal/animaciones/introLaboratorio.component.ts
+++ interficies-app/client/src/app/principal/animaciones/introLaboratorio.component.ts
@@ -1,7 +1,8 @@
-import { Component} from '@angular/core';
+import { Component, ViewChild, ElementRef} from '@angular/core';
 import { Router } from "@angular/router";
 import { UserService } from '../../models/user.service';
-import { AppComponent } from '../../app.component';
+import { MusicService } from 'src/app/models/music.service';
+import { AppComponent } from 'src/app/app.component';
 
 @Component({
   selector: 'animacion-intro-lab',
@@ -9,20 +10,49 @@ import { AppComponent } from '../../app.component';
   styleUrls: ['./primera.component.css']
 })
 
+/**
+ * Componente para manejar el comportamiento de la animación de introducción al Laboratorio
+ */
 export class IntroLaboratorioComponent {
 
-  constructor(private userService: UserService,  private router: Router, private app: AppComponent) {
+  //----------------------------------------------------------------------
+  // Atributos
+  //----------------------------------------------------------------------
+
+  @ViewChild('vid') video: ElementRef;    // Referencia al vide en el documento
+
+  //----------------------------------------------------------------------
+  // Constructor
+  //----------------------------------------------------------------------
+
+  constructor(private userService: UserService,  private router: Router, music: MusicService, private principal: AppComponent) {
     var user = userService.getUserLoggedIn();
     userService.updateRole(user.username, "Arqueologo").subscribe(response => {
       if(response["status"] == 0) {
         user.currentRol = "Arqueologo";
         userService.setUserLoggedIn(user);
-        app.updateLogin();
+        principal.updateLogin();
       }
     });
   }
 
+  //----------------------------------------------------------------------
+  // Funciones
+  //----------------------------------------------------------------------
+
+  /**
+   * Navega hacia el componente del comic, capitulos 1 a 15
+   */
   onContinue() {
     this.router.navigate(["laboratorio"]);
+  }
+
+  /**
+   * Reinicia el video
+   */
+  onAgain() {
+    this.video.nativeElement.pause();
+    this.video.nativeElement.currentTime = 0;
+    this.video.nativeElement.load();
   }
 }
